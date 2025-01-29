1d0b68bf9d2add9043c7e4f1d01e10d3a70bf991
Jefferson Reyes <Jeffersondavidreyes@gmail.com>
2025-01-29 16:14:20 -0500

    Particles.min eliminado, se está usando en su lugar el cdn

--- public/appParticles.js
+++ public/appParticles.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", "public/particles/particlesjs.json", function () {
+particlesJS.load("particles-js", "public/particlesjs.json", function () {
   console.log("particles.js config Cargado"); 
 });
