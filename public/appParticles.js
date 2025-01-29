d492ee48885476295cd7efc5f061b3ad9ee3455b
Jefferson Reyes <Jeffersondavidreyes@gmail.com>
2025-01-29 16:25:32 -0500

    eliminacion de partciles js

--- public/appParticles.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", "public/particlesjs.json", function () {
-  console.log("particles.js config Cargado"); 
-});
